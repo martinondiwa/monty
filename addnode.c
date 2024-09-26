67c895ab0a12a32a39050d0c670006673fdaa72d
martinondiwa <surekondiwa@gmail.com>
2024-09-26 15:29:29 -0700

    node

--- addnode.c
+++ addnode.c
@@ -4,27 +4,27 @@
  * addnode - Adds a node to the head of the stack.
  * @head: Pointer to the head of the stack.
  * @n: New value to be added.
- * 
+ *
  * Return: No return.
  */
 void addnode(stack_t **head, int n)
 {
-    stack_t *new_node, *aux;
+	stack_t *new_node, *aux;
 
-    aux = *head;
-    new_node = malloc(sizeof(stack_t));
-    if (new_node == NULL)
-    {
-        printf("Error\n");
-        exit(0);
-    }
+	aux = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+		exit(0);
+	}
 
-    if (aux)
-        aux->prev = new_node;
+	if (aux)
+		aux->prev = new_node;
 
-    new_node->n = n;
-    new_node->next = *head;
-    new_node->prev = NULL;
-    *head = new_node;
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	*head = new_node;
 }
 
