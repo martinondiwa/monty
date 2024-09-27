a83259c89ad0bcbb84683cf9f6b5802e82dce047
martinondiwa <surekondiwa@gmail.com>
2024-09-26 21:55:00 -0700

    asd

--- rotr.c
+++ rotr.c
@@ -4,7 +4,7 @@
  * f_rotr - Rotates the stack to the bottom.
  * @head: Pointer to the stack head.
  * @counter: Line number (unused).
- * 
+ *
  * Return: No return.
  */
 void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter)
