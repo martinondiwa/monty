5bb4bb99a75357e752d26f1dd177d4b6a228128a
martinondiwa <surekondiwa@gmail.com>
2024-09-26 22:22:20 -0700

    ff

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
+#define _GNU_SOURCE
 #include "monty.h"
-bus_t bus = {NULL, NULL, NULL, 0};
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
-* Return: 0 on success
-*/
+ * bus_t struct should be declared here if not included in monty.h
+ */
+
+bus_t bus = {NULL, NULL, NULL, 0};
+
+/**
+ * main - monty code interpreter
+ * @argc: number of arguments
+ * @argv: monty file location
+ * Return: 0 on success
+ */
 int main(int argc, char *argv[])
 {
 	char *content;
@@ -20,13 +29,16 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
+
 	file = fopen(argv[1], "r");
 	bus.file = file;
+
 	if (!file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
+
 	while (read_line > 0)
 	{
 		content = NULL;
@@ -39,7 +51,9 @@ int main(int argc, char *argv[])
 		}
 		free(content);
 	}
+
 	free_stack(stack);
 	fclose(file);
-return (0);
+	return (0);
 }
+
