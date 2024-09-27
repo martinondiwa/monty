ea6e1a836c2ae2c3e4eb31938647c5573b6ca189
martinondiwa <surekondiwa@gmail.com>
2024-09-26 22:00:44 -0700

    sdf

---
+++ stack.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * f_stack - Sets the data structure to stack mode.
+ * @head: Pointer to the stack head.
+ * @counter: Line number (unused).
+ *
+ * Return: No return.
+ */
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
+
