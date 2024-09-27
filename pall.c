1b6cace6b788f2aadd37aca933ebdb35a8b92127
martinondiwa <surekondiwa@gmail.com>
2024-09-26 21:07:35 -0700

    pal

---
+++ pall.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * f_pall - Prints the stack.
+ * @head: Pointer to the stack head.
+ * @counter: Not used.
+ * 
+ * Return: No return.
+ */
+void f_pall(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	(void)counter;
+
+	h = *head;
+	if (h == NULL)
+		return;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+}
+
