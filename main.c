88528abe223b693238e245a9ce8962b6e85a0fdd
martinondiwa <surekondiwa@gmail.com>
2024-09-26 22:11:14 -0700

    mai

--- main.c
+++ main.c
@@ -1,16 +1,13 @@
 #include "monty.h"
-
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
- * main - Monty code interpreter.
- * @argc: Number of arguments.
- * @argv: Monty file location.
- *
- *
- *Return: 0 on success.
- */
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
+*/
 int main(int argc, char *argv[])
 {
-	bus_t bus = {NULL, NULL, NULL, 0};  /* Moved inside main */
 	char *content;
 	FILE *file;
 	size_t size = 0;
@@ -23,7 +20,6 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-
 	file = fopen(argv[1], "r");
 	bus.file = file;
 	if (!file)
@@ -31,7 +27,6 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-
 	while (read_line > 0)
 	{
 		content = NULL;
@@ -44,9 +39,7 @@ int main(int argc, char *argv[])
 		}
 		free(content);
 	}
-
 	free_stack(stack);
 	fclose(file);
-	return (0);
+return (0);
 }
-
